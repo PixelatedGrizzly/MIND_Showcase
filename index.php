11cee266c96e30bd9af8286a35bc52540573d864
Cyrille Vanhulle <Cyrille Vanhulle>
2017-05-29 22:42:03 +0200

    Caroussel add

--- index.php
+++ index.php
@@ -1,8 +1,55 @@
+
+
 <?php
 include_once('view/header.php');
 ?>
 
+<script>
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    items:3,
+    animateOut: 'fadeOut',
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:false,
+    margin:10,
+    nav:true,
+    responsive:{
+        1000:{
+            items:1
+        },
+        2000:{
+            items:3
+        },
+        3000:{
+            items:5
+        }
+    }
+  })
+  $('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+  })
+  $('.stop').on('click',function(){
+      owl.trigger('stop.owl.autoplay')
+  })
+});
+</script>
 
+<div class="container">
+  <div class="row">
+    <div class="owl-carousel ">
+      <div class="slide">
+        <img src="media/image/1.jpg" alt="projet1">
+      </div>
+      <div class="slide">
+        <img src="media/image/2.jpg" alt="projet2">
+      </div>
+      <div class="slide">
+        <img src="media/image/3.jpg" alt="projet3">
+      </div>
+    </div>
+  </div>
+</div>
 
 <?php
 include_once('view/footer.php');
--- view/header.php
+++ view/header.php
@@ -5,6 +5,12 @@
     <link href="lib/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
     <title>MIND Showcase</title>
     <link rel="stylesheet" href="./media/styles.css">
+
+    <link rel="stylesheet" href="lib/OwlCarousel2-2.2.1/dist/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="lib/OwlCarousel2-2.2.1/dist/assets/owl.theme.default.min.css">
+    <script type="text/javascript" src="lib\jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="lib\bootstrap-3.3.7-dist\js\bootstrap.js"></script>
+    <script type="text/javascript" src="lib\OwlCarousel2-2.2.1\dist\owl.carousel.min.js"></script>
   </head>
   <body>
 
@@ -15,17 +21,49 @@
     <div class="container">
 <?php } ?>
 
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">MIND Showcase</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a href="#">Nouveau Projet</a></li>
+      </ul>
+      <form class="navbar-form navbar-left">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Search</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Connexion</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+
 
 <?php
 include_once('datasource/connect.php');
 $db = new ConnectionInstance();
 $usersList = $db->selectAllUsers();
 
-foreach ($usersList as $user) {
+/**foreach ($usersList as $user) {
   print(
     "<p>Nom = ".$user['nom_Util']." ; Prénom = ".$user['prenom_Util']." ; Login = ".$user['login_Util']."</p>"
   );
-}
+}**/
 ?>
 
 <div class="project_list">
