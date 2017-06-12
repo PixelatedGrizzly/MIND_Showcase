ed6f28c4bc973d04dd3177042519459ca4fc7f72
Meklo <marcellinhugo@orange.fr>
2017-06-12 09:50:07 +0200

    Ajout du design pattern Idenpotent retry au service connector, cela lui permet d'effectuer trois essais de connexion pour chaque requêtes

--- serviceConnector/serviceConnector.js
+++ serviceConnector/serviceConnector.js
@@ -1,27 +1,35 @@
-
 class ServiceConnector {
-    constructor(_address, _method, _data){
-        this.data = _data;
-        this.address = _address;
-        this.method = _method;
-        this.result = "";
-    }
+  constructor(_address, _method, _data) {
+    this.data = _data;
+    this.address = _address;
+    this.method = _method;
+    this.result = "";
 
-    sendRequest(){
-        var jqxhr = $.ajax({
+    this.maxAttempt = 3;
+  }
+
+  sendRequest() {
+
+    if (maxAttempt > 0) {
+      var jqxhr = $.ajax({
           url: this.address,
           async: true,
-          data : this.data,
-          method : this.method
-        } )
-  .done(function(res) {
-    console.log(res.body);
-  })
-  .fail(function() {
-    
-  })
-  .always(function() {
+          data: this.data,
+          method: this.method
+        })
+        .done(function(res) {
+          console.log(res.body);
+        })
+        .fail(function() {
+          maxAttempt--;
+          this.sendRequest();
+          console.log("Connection to " + address + " failed, " + maxAttempt + " attempts left...")
+        })
+        .always(function() {
 
-  });
+        });
     }
+  }else {
+    this.maxAttempt = 3;
+  }
 }
